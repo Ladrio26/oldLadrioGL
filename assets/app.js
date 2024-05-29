a8b9512cdfba12299c9760643cbdfdbcad86ff6f
Ladrio <ladrio@hotmail.fr>
2024-05-29 23:37:02 +0200

    Visuel navbar

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,11 +17,9 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        
+        $error = $authenticationUtils->getLastAuthenticationError(); // get the login error if there is one
+        $lastUsername = $authenticationUtils->getLastUsername(); // last username entered by the user
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
