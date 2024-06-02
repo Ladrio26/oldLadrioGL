7a441c8a9f63ffc442494b577d1a98b1fa2e3987
Ladrio <ladrio@hotmail.fr>
2024-06-02 02:33:02 +0200

    Réorganisation CSS et changements de style du site

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,19 +15,24 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', TextType::class)
-            ->add('email')
+            ->add('username', TextType::class, [
+                'label' => 'registration_form.username',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'registration_form.email',
+            ])
             ->add('Password', PasswordType::class, [
+                'label' => 'registration_form.password',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'translation_domain' => 'messages',
         ]);
     }
 }
