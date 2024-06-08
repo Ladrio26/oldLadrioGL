12ebf4f0f5fe24f20d4e3b996ddc496a343f9764
Ladrio <ladrio@hotmail.fr>
2024-06-08 04:04:55 +0200

    Style pour les pages de jeux indiv

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,5 +1,7 @@
 <?php
 
+// src/Controller/SecurityController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,17 +13,14 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use App\Form\ResetPasswordRequestFormType;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $error = $authenticationUtils->getLastAuthenticationError(); 
-        $lastUsername = $authenticationUtils->getLastUsername(); 
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
@@ -58,71 +57,6 @@ class SecurityController extends AbstractController
 
         return $this->render('security/register.html.twig', [
             'registrationForm' => $form->createView(),
-        ]);
-    }
-
-    #[Route(path: '/reset-password', name: 'app_forgot_password_request')]
-    public function request(Request $request, MailerInterface $mailer, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(ResetPasswordRequestFormType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $email = $form->get('email')->getData();
-            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-            if ($user) {
-                $resetToken = bin2hex(random_bytes(32));
-                $user->setResetToken($resetToken);
-                $entityManager->flush();
-
-                $email = (new Email())
-                    ->from('no-reply@example.com')
-                    ->to($user->getEmail())
-                    ->subject('Password reset request')
-                    ->html($this->renderView('emails/reset_password.html.twig', [
-                        'resetToken' => $resetToken,
-                    ]));
-
-                $mailer->send($email);
-            }
-
-            $this->addFlash('success', 'Un email de réinitialisation de mot de passe a été envoyé.');
-            return $this->redirectToRoute('app_login');
-        }
-
-        return $this->render('security/reset_password_request.html.twig', [
-            'requestForm' => $form->createView(),
-        ]);
-    }
-
-    #[Route(path: '/reset-password/{token}', name: 'app_reset_password')]
-    public function reset(Request $request, string $token, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
-    {
-        $user = $entityManager->getRepository(User::class)->findOneBy(['resetToken' => $token]);
-
-        if (!$user) {
-            $this->addFlash('error', 'Invalid reset token.');
-            return $this->redirectToRoute('app_login');
-        }
-
-        if ($request->isMethod('POST')) {
-            $newPassword = $request->request->get('new_password');
-            $user->setPassword(
-                $passwordHasher->hashPassword(
-                    $user,
-                    $newPassword
-                )
-            );
-            $user->setResetToken(null);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Votre mot de passe a été réinitialisé avec succès.');
-            return $this->redirectToRoute('app_login');
-        }
-
-        return $this->render('security/reset_password.html.twig', [
-            'token' => $token,
         ]);
     }
 }
